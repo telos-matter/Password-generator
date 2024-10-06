e3618810a260395433acbb17d08c8b0b074d0c2e
HEMMOUDA Aymane <ahemmouda@gmail.com>
2024-10-06 11:55:48 +0200

    Update Interface.java

--- src/main/java/hemmouda/passwordgenerator/Interface.java
+++ src/main/java/hemmouda/passwordgenerator/Interface.java
@@ -1,8 +1,8 @@
 package hemmouda.passwordgenerator;
 
-import java.awt.BorderLayout;
-import java.awt.Container;
-import java.awt.GridLayout;
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -13,19 +13,23 @@ import javax.swing.JToggleButton;
 import javax.swing.event.ChangeEvent;
 
 public class Interface {
-	
-	private static JToggleButton upper_case;
-	private static JToggleButton lower_case;
-	private static JToggleButton numbers;
-	private static JToggleButton special_char;
-	private static JSlider slider;
-	private static JButton generate;
-	
-	private static JTextField field;
+
+	private static final Clipboard CLIPBOARD = Toolkit.getDefaultToolkit().getSystemClipboard();
+
+	private static JToggleButton UPPER_CASE;
+	private static JToggleButton LOWER_CASE;
+	private static JToggleButton NUMBERS;
+	private static JToggleButton SPECIAL_CHARS;
+	private static JSlider SLIDER;
+
+	private static JButton GENERATE_BUTTON;
+	private static final String GENERATE_BUTTON_TEXT = "Generate a new %d char long password";
+
+	private static JTextField TEXT_FIELD;
 
 	public static void initialize () {
 		JFrame frame = new JFrame ();
-		frame.setTitle("Password generator");
+		frame.setTitle("Password generator - automatically copied");
 		frame.setSize(900, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
@@ -35,92 +39,70 @@ public class Interface {
 		JPanel panel = new JPanel ();
 		panel.setLayout(new GridLayout (3, 2));
 		
-		upper_case = new JToggleButton ();
-		upper_case.setFocusable(false);
-		upper_case.setSelected(true);
-		upper_case.setText("Upper case letters");
-		upper_case.addActionListener((ActionEvent e) -> {
-			Generator.generate();
-		});
-		panel.add(upper_case);
+		UPPER_CASE = createJToggleButton("Upper case letters");
+		panel.add(UPPER_CASE);
 		
-		lower_case = new JToggleButton ();
-		lower_case.setFocusable(false);
-		lower_case.setSelected(true);
-		lower_case.setText("Lower case letters");
-		lower_case.addActionListener((ActionEvent e) -> {
-			Generator.generate();
-		});
-		panel.add(lower_case);
+		LOWER_CASE = createJToggleButton("Lower case letters");
+		panel.add(LOWER_CASE);
 		
-		numbers = new JToggleButton ();
-		numbers.setFocusable(false);
-		numbers.setSelected(true);
-		numbers.setText("Numbers");
-		numbers.addActionListener((ActionEvent e) -> {
-			Generator.generate();
-		});
-		panel.add(numbers);
+		NUMBERS = createJToggleButton("Numbers");
+		panel.add(NUMBERS);
 		
-		special_char = new JToggleButton ();
-		special_char.setFocusable(false);
-		special_char.setSelected(true);
-		special_char.setText("Special characters");
-		special_char.addActionListener((ActionEvent e) -> {
-			Generator.generate();
-		});
-		panel.add(special_char);
+		SPECIAL_CHARS = createJToggleButton("Special characters");
+		panel.add(SPECIAL_CHARS);
 		
-		slider = new JSlider (JSlider.HORIZONTAL, 1, 64, 16);
-		slider.setFocusable(false);
-		slider.setMajorTickSpacing(3);
-		slider.setPaintTicks(true);
-		slider.addChangeListener((ChangeEvent e) -> {
-			Generator.generate();
+		SLIDER = new JSlider (JSlider.HORIZONTAL, 1, 64, 16);
+		SLIDER.setFocusable(false);
+		SLIDER.setMajorTickSpacing(4);
+		SLIDER.setPaintTicks(true);
+		SLIDER.addChangeListener((ChangeEvent e) -> {
+			generatePassword();
 		});
-		panel.add(slider);
+		panel.add(SLIDER);
 		
-		generate = new JButton ();
-		generate.setText("Generate");
-		generate.setFocusable(false);
-		generate.addActionListener((ActionEvent e) -> {
-			Generator.generate();
+		GENERATE_BUTTON = new JButton ();
+		GENERATE_BUTTON.setText(GENERATE_BUTTON_TEXT.formatted(SLIDER.getValue()));
+		GENERATE_BUTTON.setFocusable(false);
+		GENERATE_BUTTON.addActionListener((ActionEvent e) -> {
+			generatePassword();
 		});
-		panel.add(generate);
+		panel.add(GENERATE_BUTTON);
 		
 		pane.add(panel, BorderLayout.WEST);
 		
-		field = new JTextField (64);
-		field.setEditable(false);
-		field.setHorizontalAlignment(JTextField.LEFT);
-		
-		pane.add(field, BorderLayout.CENTER);
+		TEXT_FIELD = new JTextField (64);
+		TEXT_FIELD.setEditable(false);
+		TEXT_FIELD.setHorizontalAlignment(JTextField.LEFT);
+		pane.add(TEXT_FIELD, BorderLayout.CENTER);
 		
 		frame.setVisible(true);
 	}
-	
-	public static void display (String text) {
-		field.setText(text);
+
+	private static JToggleButton createJToggleButton (String label) {
+		JToggleButton toggleButton = new JToggleButton ();
+		toggleButton.setFocusable(false);
+		toggleButton.setSelected(true);
+		toggleButton.setText(label);
+		toggleButton.addActionListener((ActionEvent e) -> {
+			generatePassword();
+		});
+
+		return toggleButton;
 	}
-	
-	public static int getLength () {
-		return slider.getValue();
-	}
-	
-	public static boolean isUpper () {
-		return upper_case.isSelected();
-	}
-	
-	public static boolean isLower () {
-		return lower_case.isSelected();
-	}
-	
-	public static boolean isNumbers () {
-		return numbers.isSelected();
-	}
-	
-	public static boolean isSpecial () {
-		return special_char.isSelected();
+
+	private static void generatePassword () {
+		int length = SLIDER.getValue();
+
+		GENERATE_BUTTON.setText(GENERATE_BUTTON_TEXT.formatted(length));
+
+		String password = Generator.generate(length,
+				UPPER_CASE.isSelected(),
+				LOWER_CASE.isSelected(),
+				NUMBERS.isSelected(),
+				SPECIAL_CHARS.isSelected());
+
+		TEXT_FIELD.setText(password);
+		CLIPBOARD.setContents(new StringSelection(password), null);
 	}
 	
 }
