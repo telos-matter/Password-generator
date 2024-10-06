824059601edbe43a8a554f1c9cf387d3dad02155
HEMMOUDA Aymane <ahemmouda@gmail.com>
2024-10-06 11:55:50 +0200

    Update Main.java

--- src/main/java/hemmouda/passwordgenerator/Main.java
+++ src/main/java/hemmouda/passwordgenerator/Main.java
@@ -1,10 +1,16 @@
 package hemmouda.passwordgenerator;
 
+import javax.swing.*;
+
 public class Main {
 
 	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception ignored) {}
+		ToolTipManager.sharedInstance().setDismissDelay(Integer.MAX_VALUE);
+
 		Interface.initialize();
-		Generator.generate();
 	}
 
 }
